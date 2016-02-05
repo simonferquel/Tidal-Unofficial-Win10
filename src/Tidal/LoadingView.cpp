2318d87cfd851b26e03cc7a4bc36a145d37ce7f7
Simon Ferquel <sferquel@infinitesquare.com>
2016-02-05 15:03:50 +0100

    missing files

---
+++ src/Tidal/LoadingView.cpp
@@ -0,0 +1,101 @@
+﻿//
+// LoadingView.cpp
+// Implementation of the LoadingView class.
+//
+
+#include "pch.h"
+#include "LoadingView.h"
+
+using namespace Tidal;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Documents;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Interop;
+using namespace Windows::UI::Xaml::Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+LoadingView::LoadingView()
+{
+	DefaultStyleKey = "Tidal.LoadingView";
+	Loaded += ref new Windows::UI::Xaml::RoutedEventHandler(this, &Tidal::LoadingView::OnLoaded);
+}
+
+DependencyProperty^ LoadingView::_contentProperty = DependencyProperty::Register(
+	L"Content", 
+	Object::typeid, 
+	LoadingView::typeid, 
+	PropertyMetadata::Create(static_cast<Platform::Object^>(nullptr), 
+		ref new PropertyChangedCallback([](DependencyObject^ sender, DependencyPropertyChangedEventArgs^ args) {
+})));
+
+
+DependencyProperty^ LoadingView::_loadingStateProperty = DependencyProperty::Register(
+	L"LoadingState",
+	Tidal::LoadingState::typeid,
+	LoadingView::typeid,
+	PropertyMetadata::Create(Tidal::LoadingState::Loading,
+		ref new PropertyChangedCallback([](DependencyObject^ sender, DependencyPropertyChangedEventArgs^ args) {
+	auto state = dynamic_cast<IBox<Tidal::LoadingState>^>(args->NewValue)->Value;
+	Platform::String^ stateName = L"Loading";
+	switch (state)
+	{
+	case Tidal::LoadingState::Loaded:
+		stateName = L"Loaded";
+		break;
+	case Tidal::LoadingState::Loading:
+		stateName = L"Loading";
+		break;
+	case Tidal::LoadingState::Error:
+		stateName = L"Error";
+		break;
+	default:
+		break;
+	}
+	VisualStateManager::GoToState(dynamic_cast<Control^>(sender), stateName, true);
+})));
+
+
+DependencyProperty^ LoadingView::_loadingTextProperty = DependencyProperty::Register(
+	L"LoadingText",
+	String::typeid,
+	LoadingView::typeid,
+	PropertyMetadata::Create(L"Loading...",
+		ref new PropertyChangedCallback([](DependencyObject^ sender, DependencyPropertyChangedEventArgs^ args) {
+})));
+
+DependencyProperty^ LoadingView::_errorTextProperty = DependencyProperty::Register(
+	L"ErrorText",
+	String::typeid,
+	LoadingView::typeid,
+	PropertyMetadata::Create(L"An error occured. Please try again later.",
+		ref new PropertyChangedCallback([](DependencyObject^ sender, DependencyPropertyChangedEventArgs^ args) {
+})));
+
+void Tidal::LoadingView::OnLoaded(Platform::Object ^sender, Windows::UI::Xaml::RoutedEventArgs ^e)
+{
+	Platform::String^ stateName = L"Loading";
+	switch (this->LoadingState)
+	{
+	case Tidal::LoadingState::Loaded:
+		stateName = L"Loaded";
+		break;
+	case Tidal::LoadingState::Loading:
+		stateName = L"Loading";
+		break;
+	case Tidal::LoadingState::Error:
+		stateName = L"Error";
+		break;
+	default:
+		break;
+	}
+	if (Windows::ApplicationModel::DesignMode::DesignModeEnabled)
+		stateName = L"Loaded";
+	VisualStateManager::GoToState(dynamic_cast<Control^>(sender), stateName, false);
+}
---
+++ src/Tidal/LoadingView.h
@@ -0,0 +1,58 @@
+﻿//
+// LoadingView.h
+// Declaration of the LoadingView class.
+//
+
+#pragma once
+
+namespace Tidal
+{
+	public enum class LoadingState {
+		Loaded,
+		Loading,
+		Error
+	};
+
+	[Windows::UI::Xaml::Markup::ContentPropertyAttribute(Name=L"Content")]
+	public ref class LoadingView sealed : public Windows::UI::Xaml::Controls::Control
+	{
+	private:
+		static Windows::UI::Xaml::DependencyProperty^ _contentProperty;
+		static Windows::UI::Xaml::DependencyProperty^ _loadingStateProperty;
+		static Windows::UI::Xaml::DependencyProperty^ _loadingTextProperty;
+		static Windows::UI::Xaml::DependencyProperty^ _errorTextProperty;
+	public:
+		LoadingView();
+
+		static property Windows::UI::Xaml::DependencyProperty^ ContentProperty {
+			Windows::UI::Xaml::DependencyProperty^ get() { return _contentProperty; }
+		}
+		static property Windows::UI::Xaml::DependencyProperty^ LoadingStateProperty {
+			Windows::UI::Xaml::DependencyProperty^ get() { return _loadingStateProperty; }
+		}
+		static property Windows::UI::Xaml::DependencyProperty^ LoadingTextProperty {
+			Windows::UI::Xaml::DependencyProperty^ get() { return _loadingTextProperty; }
+		}
+		static property Windows::UI::Xaml::DependencyProperty^ ErrorTextProperty {
+			Windows::UI::Xaml::DependencyProperty^ get() { return _errorTextProperty; }
+		}
+
+		property Platform::Object^ Content {
+			Platform::Object^ get() { return GetValue(ContentProperty); }
+			void set(Platform::Object^ value) { SetValue(ContentProperty, value); }
+		}
+		property Tidal::LoadingState LoadingState {
+			Tidal::LoadingState get() { return dynamic_cast<Platform::IBox<Tidal::LoadingState>^>(GetValue(LoadingStateProperty))->Value; }
+			void set(Tidal::LoadingState value) { SetValue(LoadingStateProperty, value); }
+		}
+		property Platform::String^ LoadingText {
+			Platform::String^ get() { return dynamic_cast<Platform::String^>(GetValue(LoadingTextProperty)); }
+			void set(Platform::String^ value) { SetValue(LoadingTextProperty, value); }
+		}
+		property Platform::String^ ErrorText {
+			Platform::String^ get() { return dynamic_cast<Platform::String^>(GetValue(ErrorTextProperty)); }
+			void set(Platform::String^ value) { SetValue(ErrorTextProperty, value); }
+		}
+		void OnLoaded(Platform::Object ^sender, Windows::UI::Xaml::RoutedEventArgs ^e);
+	};
+}
